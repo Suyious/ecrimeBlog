8ed7117ebc161bee0560db8fd04d9d3c97744da2
Suyash <suyu162001@gmail.com>
2021-05-16 15:57:52 +0530

    Responsive

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function BlogCard({param, cardImg, Title, body}) {
-    return (
-        <Link className="BlogLink boxwidth" to={`blog/${param}`}>
-            <div className="BlogCard">
-                <div className="cardImg">
-                    <img className="cardCover" src={cardImg} alt=""/>
-                </div>
-                <div className="cardContent">
-                    <div className="cardheads">
-                        <h2>{Title}</h2>
-                        <p>{body.substr(0,500)}...</p>
-                    </div>
-                    <a href="//">Visit Blog ➜</a>
-                </div>
-            </div>
-        </Link>
-    )
+function BlogCard({ param, cardImg, Title, body }) {
+  return (
+    <Link className="BlogLink boxwidth" to={`blog/${param}`}>
+      <div className="BlogCard">
+        <div className="cardImg">
+          <img className="cardCover" src={cardImg} alt="" />
+        </div>
+        <div className="cardContent">
+          <div className="cardheads">
+            <h2>{Title}</h2>
+            <p>{body.substr(0, 500)}...</p>
+          </div>
+          <a href="//">Visit Blog ➜</a>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
-export default BlogCard
+export default BlogCard;
